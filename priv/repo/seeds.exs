a04b2455196b5ec06afe89dff00f5edc9a3600cb
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2024-01-08 23:51:25 -0600

    Add seed data for products

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias WhiteRabbitServer.Repo
+alias WhiteRabbitServer.Catalog.Product
+
+Repo.insert!(%Product{
+  sku: "RMJ00001",
+  name: "1950’s Solid Brass Golden Retriever Music Box made by Leonard Silver Co.",
+  description:
+    "This rare find is in working condition. Some patina on one side of the dogs face. Beautiful piece to add to your shelves or a child’s room.",
+  size: "about 4 1/2 inches tall",
+  price: "42",
+  url:
+    "https://www.whiterabbitvintagemarket.com/images/products/brass-golden-retriever-music-box.jpeg",
+  is_sold: false
+})

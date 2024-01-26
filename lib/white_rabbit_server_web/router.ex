d52c21b39720b30bf19f34983991f21731b2b158
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2024-01-25 22:36:18 -0600

    Add api to fetch a single order

--- lib/white_rabbit_server_web/router.ex
+++ lib/white_rabbit_server_web/router.ex
@@ -8,6 +8,7 @@ defmodule WhiteRabbitServerWeb.Router do
   scope "/api", WhiteRabbitServerWeb do
     pipe_through :api
     get "/products", ProductController, :index
+    get "/orders", OrderController, :index
     post "/shopping-cart/begin-checkout", ShoppingCartController, :create
     post "/shopping-cart/complete-checkout", ShoppingCartController, :update
   end

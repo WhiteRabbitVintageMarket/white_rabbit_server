1768fd43db8e743b8781d5e6496a42e3340b2106
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2024-08-19 22:17:08 -0500

    Add new route for browser safe access token

---
+++ lib/white_rabbit_server_web/controllers/auth_json.ex
@@ -0,0 +1,5 @@
+defmodule WhiteRabbitServerWeb.AuthJSON do
+  def index(%{response: response}) do
+    response
+  end
+end
--- lib/white_rabbit_server_web/router.ex
+++ lib/white_rabbit_server_web/router.ex
@@ -9,6 +9,7 @@ defmodule WhiteRabbitServerWeb.Router do
     pipe_through :api
     get "/products", ProductController, :index
     get "/orders", OrderController, :index
+    get "/browser-safe-client-token", AuthController, :index
     post "/shopping-cart/begin-checkout", ShoppingCartController, :create
     post "/shopping-cart/complete-checkout", ShoppingCartController, :update
   end
